a6993c28143d9f163f2e2f91863433e99657e70e
XadillaX <admin@xcoder.in>
2013-09-21 01:39:12 +0800

    revolution

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-var express = require('express');
-var index = require('./routes/index');
-var verifyapi = require("./routes/verifyapi");
-var http = require('http');
-var path = require('path');
+var wechaxServer = require("node-wechax").newServer("kacakacaANDkacakaca", 3001);
 
-var app = express();
-
-// all environments
-app.set('port', 3001);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get("/", index.index);
-app.get("/verifyapi", verifyapi.index);
-app.post("/verifyapi", verifyapi.post);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+wechaxServer.on("text", function(sender, from, to, time, text, msgid) {
+    var str = "你现在还发什么“" + text + "”啊！也不看看现在都已经" + new Date(Date.now()).getHours() + "点" + new Date(Date.now()).getMinutes() + "了！还不赶紧睡觉去！";
+    sender.sendText(to, from, str);
 });
+
+wechaxServer.start();
--- routes/index.js
+++
@@ -1,5 +0,0 @@
-var token = "kacakacaANDkacakaca";
-
-exports.index = function(req, res){
-
-};
